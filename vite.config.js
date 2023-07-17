dbb106946dfec5a1648b60bac65a43978df3cbad
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-17 16:15:20 +0200

    try fix production error

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [react()],
+	define: {'process.env.NODE_ENV': '"production"'},
 	build: {
 		outDir: "hello_widget/static",
 		lib: {
