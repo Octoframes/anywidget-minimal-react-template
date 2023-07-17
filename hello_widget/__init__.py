bfd2cb650d37827b3d33a9ffd957a78eedacdd46
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-17 14:34:38 +0200

    tweaks

--- hello_widget/__init__.py
+++ hello_widget/__init__.py
@@ -4,5 +4,6 @@ import traitlets
 
 class HelloWidget(anywidget.AnyWidget):
     _esm =  pathlib.Path(__file__).parent / "static/main.js"
-    # _css=  pathlib.Path(__file__).parent / "static/styles.css"
     count = traitlets.Int(0).tag(sync=True)
+
+    # _css=  pathlib.Path(__file__).parent / "static/styles.css"
