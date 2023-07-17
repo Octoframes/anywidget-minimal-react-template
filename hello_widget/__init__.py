9a1433d0272f21eedf0da9b979a94d77759aa1d5
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-17 13:54:01 +0200

    finish structure

--- hello_widget/__init__.py
+++ hello_widget/__init__.py
@@ -3,34 +3,6 @@ import anywidget
 import traitlets
 
 class HelloWidget(anywidget.AnyWidget):
-    _esm = """
-    export function render({ model, el }) {
-      let getCount = () => model.get("count");
-      let button = document.createElement("button");
-      button.classList.add("counter-button");
-      button.innerHTML = `count is ${getCount()}`;
-      button.addEventListener("click", () => {
-        model.set("count", getCount() + 1);
-        model.save_changes();
-      });
-      model.on("change:count", () => {
-        button.innerHTML = `count is ${getCount()}`;
-      });
-      el.appendChild(button);
-    }
-    """
-    _css="""
-    .counter-button { background-color: #ea580c; }
-    .counter-button:hover { background-color: #9a3412; }
-    """
+    _esm =  pathlib.Path(__file__).parent.parent / "src/index.js"
+    _css=  pathlib.Path(__file__).parent.parent / "src/styles.css"
     count = traitlets.Int(0).tag(sync=True)
-
-
-
-# # bundler yields hello_widget/static/{index.js,styles.css}
-# bundler_output_dir = pathlib.Path(__file__).parent / "static"
-
-# class HelloWidget(anywidget.AnyWidget):
-#   _esm = bundler_output_dir / "index.js"
-#   _css = bundler_output_dir / "styles.css"
-#   name = traitlets.Unicode().tag(sync=True)
