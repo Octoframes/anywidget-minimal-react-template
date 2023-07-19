1011f9a57069a9dfaa8b14d89dea6887f6f86a02
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-19 15:25:38 +0200

    update content anytime model changes

--- src/widget.jsx
+++ src/widget.jsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
+function WidgetAdapter({ model }) {
+  let [content, setContent] = React.useState(model.get("content"));
+  React.useEffect(() => {
+    // update content anytime the model changes (from either Python or JS)
+    model.on("change:content", () => setContent(model.get("content")));
+  }, [])
+  return <App content={content} />
+}
+
 export function render({ model, el }) {
   let root = ReactDOM.createRoot(el);
   root.render(
     <React.StrictMode>
-      <App content={model.get("content")} />
+      <WidgetAdapter model={model} />
     </React.StrictMode>,
   );
   return () => root.unmount();
