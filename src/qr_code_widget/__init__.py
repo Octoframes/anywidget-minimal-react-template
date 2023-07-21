c7294809aa19c30a8fc6d85e3a4dd6c1c94cc0e0
Trevor Manz <trevor.j.manz@gmail.com>
2023-07-21 10:52:04 -0400

    feat: expose package version to runtime

--- src/qr_code_widget/__init__.py
+++ src/qr_code_widget/__init__.py
@@ -1,7 +1,15 @@
 import pathlib
+import importlib.metadata
+
 import anywidget
 import traitlets
 
+try:
+    __version__ = importlib.metadata.version("qr_code_widget")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+
 class QRCodeWidget(anywidget.AnyWidget):
     content = traitlets.Unicode("Hi").tag(sync=True)
-    _esm =  pathlib.Path(__file__).parent / "widget.js"
+    _esm = pathlib.Path(__file__).parent / "widget.js"
